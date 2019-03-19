e113f7a191e1c9dbfc6537f8e8e74a3c638cc569
JBloodBeardN <jtn.neese@gmail.com>
2019-03-19 11:51:02 -0500

    added call to firebase, if/then for check on lastUpdate < 10minutes ago, print to HTML for one variable also working, AJAX call returning data, but AJAX not yet printing to HTML

--- OpenWeather.js
+++ OpenWeather.js
@@ -1,60 +1,56 @@
-//======Start of running script=========
+// //======Start of running script=========
 
 
-$(document).ready(function () {
+// $(document).ready(function () {
 
-    // Storing our query string for OpenWeatherMap API
+//     // Storing our query string for OpenWeatherMap API
 
-    // This is our API key
-    var APIKey = "f811d6890d096d171ee1586e5b3264b4";
-    var toLocation = localStorageObject.toCity + ",us";
-
-    // Here we are building the URL we need to query the database
-    // api.openweathermap.org/data/2.5/forecast?q=London,us&mode=xml
-    console.log(24 * 60 * 60 * 100);
-    console.log(Date.now());
-
-    var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=Austin,us&APPID=f811d6890d096d171ee1586e5b3264b4&units=imperial";
-    // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=f811d6890d096d171ee1586e5b3264b4";
-
-    // Perfoming an AJAX GET request to our queryURL
-    $.ajax({
-        url: queryURL,
-        method: "GET"
-    })
-
-        // After the data from the AJAX request comes back
-        .then(function (response) {
-
-            // Saving the response object (and console log for object check)
-            var newResponse = response;
-            console.log(response);
-            console.log(newResponse);
-            var newDiv = $("<div id='weatherPrintDiv'></div>");
-
-            //=======================================================
-            //=======you have to traverse the forecast the get the most accurate data for now
-            //=======the API array starts at [0] = today @ 00:00:00
-
-            // Creating Elements and storing the text data
-            var printArray = ["temp", "humidity", "description"]
-            printArray.forEach(function (element) {
-
-                var newP = $("<p>");
-
-                newP.text("test " + element);
+//     // This is our API key
+//     var APIKey = "f811d6890d096d171ee1586e5b3264b4";
+//     var toLocation = localStorageObject.toCity + ",us";
 
 
-                newDiv.append(newP);
-            })
-            for (i = 0; i < 39; i++) {
-                newDiv.append('<p>' + JSON.stringify(newResponse.list[i]) + '</p>');
-            };
+//     var queryURL = "https://api.openweathermap.org/data/2.5/forecast?q=Austin,us&APPID=f811d6890d096d171ee1586e5b3264b4&units=imperial";
+//     // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=f811d6890d096d171ee1586e5b3264b4";
 
-            // Prepending the created elements to the HTML.document
-            $("#idOFWhatever").append(newDiv);
-        });
-});
+//     // Perfoming an AJAX GET request to our queryURL
+//     $.ajax({
+//         url: queryURL,
+//         method: "GET"
+//     })
+
+//         // After the data from the AJAX request comes back
+//         .then(function (response) {
+
+//             // Saving the response object (and console log for object check)
+//             var newResponse = response;
+//             console.log(response);
+//             console.log(newResponse);
+//             var newDiv = $("<div id='weatherPrintDiv'></div>");
+
+//             //=======================================================
+//             //=======you have to traverse the forecast the get the most accurate data for now
+//             //=======the API array starts at [0] = today @ 00:00:00
+
+//             // Creating Elements and storing the text data
+//             var printArray = ["temp", "humidity", "description"]
+//             printArray.forEach(function (element) {
+
+//                 var newP = $("<p>");
+
+//                 newP.text("test " + element);
+
+
+//                 newDiv.append(newP);
+//             })
+//             for (i = 0; i < 39; i++) {
+//                 newDiv.append('<p>' + JSON.stringify(newResponse.list[i]) + '</p>');
+//             };
+
+//             // Prepending the created elements to the HTML.document
+//             $("#idOFWhatever").append(newDiv);
+//         });
+// });
 
 
 // ==============start of psuedocode comments, as if I had not done any development above
@@ -97,60 +93,173 @@ var day5 = {
     date: ""
 };
 
-        // function updatePrintDisplay(object);
-        //this updates the html text display
+// function updatePrintDisplay(object);
+//this updates the html text display
 
-        //function updateChartJS();
-        //this pushes data into the forecastChart
+//function updateChartJS();
+//this pushes data into the forecastChart
 
-        // function updateUmbrellaAlertDisplay();
-        //this updates the condition icon and text alert display
+// function updateUmbrellaAlertDisplay();
+//this updates the condition icon and text alert display
 
-        //function parseForeCastDataIntoArray();
-        //this takes the API data and splits it into arrays per desired data type
+//function parseForeCastDataIntoArray();
+//this takes the API data and splits it into arrays per desired data type
 
-        //function parseMaxTempArray();
-        //this takes the max temp array and finds the greatest value
+//function parseMaxTempArray();
+//this takes the max temp array and finds the greatest value
 
-        //function parseMinTempArray();
-        //this takes the min temp array and finds the least value
+//function parseMinTempArray();
+//this takes the min temp array and finds the least value
 
-        //function parseConditionsArray();
-        //this compares the condition array with an indexOf check for rain, snow or fog and updates umbrellaDisplay
+//function parseConditionsArray();
+//this compares the condition array with an indexOf check for rain, snow or fog and updates umbrellaDisplay
 
-        //function turnDegreesIntoCardinalDirection();
-        //this turns a degree returned into a N, NW, etc..
+//function turnDegreesIntoCardinalDirection();
+//this turns a degree returned into a N, NW, etc..
 
+var config = {
+    apiKey: "AIzaSyBqq61A0kK_3nScseexY0EAY26DBym3s7c",
+    authDomain: "firstteamproject-16be1.firebaseapp.com",
+    databaseURL: "https://firstteamproject-16be1.firebaseio.com",
+    projectId: "firstteamproject-16be1",
+    storageBucket: "firstteamproject-16be1.appspot.com",
+    messagingSenderId: "546682156707"
+};
 
-        // document.ready function for any print to screen functions
+firebase.initializeApp(config);
+var database = firebase.database();
 
+var localStorageObject = window.localStorage;
 
+// document.ready function for any print to screen function
+$(document).ready(function () {
 
+    var cityConnection = database.ref("/" + localStorageObject.toCity)
+
+    //test harness for pushing data
+    //always false data
+    // cityConnection.set({
+    //     lastUpdate: Date.now(),
+    // });
+
+    //always true data
+    cityConnection.set({
+        lastUpdate: $.now() + (1000 * 60 * 10.5),
+        sunset: 1553042478,
+        description: "broken clouds",
+        icon: "04d",
+        id: 803,
+        main: "Clouds",
+        temp: 71.35,
+        pressure: 1026,
+        humidity: 36,
+        speed: 4.7,
+        clouds: 24,
+
+    });
+
+    cityConnection.on("value", function (snapshot) {
         //firebase inspect city last updated timestamp compare with Date.now()+10minutes
+        
+        // Log everything that's coming out of snapshot
+        console.log(snapshot.val());
+        console.log(snapshot.val().lastUpdate);
+        console.log($.now());
+        console.log(snapshot.val().lastUpdate > $.now() + (1000 * 60 * 10));
+
+        if (snapshot.val().lastUpdate > $.now() + (1000 * 60 * 10)){
         //if less than = use firebase data
-        //if greater than = use Ajax call mixed with firebase data for today(); error handle for display to current data;
+        var newCollection = $('<div>');
+        newCollection.append($('<p>'+snapshot.val().temp+'</p>'));
 
-        //ajax call actually goes here/
-        // Ajax call to get data, then function calls printDisplay functions
+        $('#parentDiv').append(newCollection);
 
-        // Ajax call to get current data to update text display
-        // current weather data retreives
-        //console.log to start and inspect for the following data
-        // rain in last hour 
-        // rain in last three hours 
-        // snow in last hour 
-        // snow in last three hours
-        // sys.sunset
-        // weather.description
-        // weather.icon
-        // weather.id 
-        // weather.main
-        // main.temp
-        // main.pressure
-        // main.humidity
-        // wind.speed
-        // wind.degs
-        // clouds.all
+        } else {
+            //if greater than = use Ajax call mixed with firebase data for today(); error handle for display to current data;
+            //ajax call actually goes here/
+            // Ajax call to get data, then function calls printDisplay functions
+
+            // Ajax call to get current data to update text display
+            // current weather data retreives
+            //console.log to start and inspect for the following data
+            // rain in last hour 
+            // rain in last three hours 
+            // snow in last hour 
+            // snow in last three hours
+            // sys.sunset
+            // weather.description
+            // weather.icon
+            // weather.id 
+            // weather.main
+            // main.temp
+            // main.pressure
+            // main.humidity
+            // wind.speed
+            // wind.degs
+            // clouds.all
+
+            // Storing our query string for OpenWeatherMap API
+
+            // This is our API key
+            var APIKey = "f811d6890d096d171ee1586e5b3264b4";
+            var toLocation = localStorageObject.toCity + ",us";
+
+            var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=Austin,us&APPID=f811d6890d096d171ee1586e5b3264b4&units=imperial";
+            // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=f811d6890d096d171ee1586e5b3264b4";
+
+            // Perfoming an AJAX GET request to our queryURL
+            $.ajax({
+                url: queryURL,
+                method: "GET"
+            })
+
+                // After the data from the AJAX request comes back
+                .then(function (response) {
+
+                    // Saving the response object (and console log for object check)
+                    var newResponse = response;
+                    console.log(response);
+                    console.log(newResponse);
+
+                    var newCollection = $('<div>');
+                    newCollection.append($('<p>'+response.main.temp+'</p>'));
+
+                    $('#parentDiv').append(newCollection);
+
+                    //=======================================================
+                    //=======you have to traverse the forecast the get the most accurate data for now
+                    //=======the API array starts at [0] = today @ 00:00:00
+
+                    // Creating Elements and storing the text data
+                    // var printArray = ["temp", "humidity", "description"]
+                    // printArray.forEach(function (element) {
+
+                    //     var newP = $("<p>");
+
+                    //     newP.text("test " + element);
+
+
+                    //     newDiv.append(newP);
+                    // })
+                    // for (i = 0; i < 39; i++) {
+                    //     newDiv.append('<p>' + JSON.stringify(newResponse.list[i]) + '</p>');
+                    // };
+
+                    // // Prepending the created elements to the HTML.document
+                    // $("#idOFWhatever").append(newDiv);
+                });
+
+        }
+    });
+
+
+
+
+});
+
+
+
+
 
 
 
@@ -169,6 +278,46 @@ var day5 = {
         //=====================================================================================
 
         // Ajax call for forecast
+        // var ForecastQueryURL = "https://api.openweathermap.org/data/2.5/weather?q=Austin,us&APPID=f811d6890d096d171ee1586e5b3264b4&units=imperial";
+        // var queryURL = "https://api.openweathermap.org/data/2.5/weather?q=London,uk&APPID=f811d6890d096d171ee1586e5b3264b4";
+
+        // Perfoming an AJAX GET request to our queryURL
+        // $.ajax({
+        //     url: forecastQueryURL,
+        //     method: "GET"
+        // })
+
+            // After the data from the AJAX request comes back
+            // .then(function (response) {
+
+            //     // Saving the response object (and console log for object check)
+            //     var newResponse = response;
+            //     console.log(response);
+            //     console.log(newResponse);
+
+
+                //=======================================================
+                //=======you have to traverse the forecast the get the most accurate data for now
+                //=======the API array starts at [0] = today @ 00:00:00
+
+                // Creating Elements and storing the text data
+                // var printArray = ["temp", "humidity", "description"]
+                // printArray.forEach(function (element) {
+
+                //     var newP = $("<p>");
+
+                //     newP.text("test " + element);
+
+
+                //     newDiv.append(newP);
+                // })
+                // for (i = 0; i < 39; i++) {
+                //     newDiv.append('<p>' + JSON.stringify(newResponse.list[i]) + '</p>');
+                // };
+
+                // // Prepending the created elements to the HTML.document
+                // $("#idOFWhatever").append(newDiv);
+            // });
         //loop through array returned and build arrays for maxTemp minTemp and Conditions
         //set return to var parseMaxTempArray(date, arrayObject);
         //set return to var parseMinTempArray(date, arrayObject);
@@ -188,6 +337,9 @@ var day5 = {
 
         //for firebase return of condition = adverse condition update umbrellaicon and umbrellaText
         //call updateChartJS(object of data)
+
+
+        // ==============================END of second AJAX call - forecast =============================================================
 
 
         // inspect returned info for umbrella alert update
