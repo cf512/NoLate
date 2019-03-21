64a008941ebed414ad9d21d6dd6a8364cbd3ffb1
JBloodBeardN <jtn.neese@gmail.com>
2019-03-21 10:08:46 -0500

    before stash, in case i bork this

--- assets/javascript/testHarness.js
+++ assets/javascript/testHarness.js
@@ -1,15 +1,15 @@
 //fake data to LocalStorage
 localStorage.setItem('name','Robert Paulson');
-localStorage.setItem('fromAddress','2405 Robert Dedman Dr');
+localStorage.setItem('fromAddress','2405 Robert Dedman Dr, Austin, TX, US');
 localStorage.setItem('fromCity','Austin');
-localStorage.setItem('fromState','TX');
-localStorage.setItem('fromZip','78712');
-localStorage.setItem('fromLocationString', '2405 Robert Dedman Dr Austin, TX 78712')
-localStorage.setItem('toAddress','11411 Research Blvd');
+// localStorage.setItem('fromState','TX');
+// localStorage.setItem('fromZip','78712');
+// localStorage.setItem('fromLocationString', '2405 Robert Dedman Dr Austin, TX 78712')
+localStorage.setItem('toAddress','11411 Research Blvd, Houston, TX, US');
 localStorage.setItem('toCity','Houston');
-localStorage.setItem('toState','TX');
-localStorage.setItem('toZip','78759');
-localStorage.setItem('toLocationString','11411 Research Blvd Austin, TX 78759');
+// localStorage.setItem('toState','TX');
+// localStorage.setItem('toZip','78759');
+// localStorage.setItem('toLocationString','11411 Research Blvd Austin, TX 78759');
 
 localStorage.setItem('requiredArrivalTime','09:00 AM');
 localStorage.setItem('morningRoutineTime','20');
@@ -25,14 +25,14 @@ var cookieObject = document.cookie;
 console.log(localStorageObject.name);
 console.log(localStorageObject.fromAddress);
 console.log(localStorageObject.fromCity);
-console.log(localStorageObject.fromState);
-console.log(localStorageObject.fromZip);
-console.log(localStorageObject.fromLocationString);
+// console.log(localStorageObject.fromState);
+// console.log(localStorageObject.fromZip);
+// console.log(localStorageObject.fromLocationString);
 console.log(localStorageObject.toAddress);
 console.log(localStorageObject.toCity);
-console.log(localStorageObject.toState);
-console.log(localStorageObject.toZip);
-console.log(localStorageObject.toLocationString);
+// console.log(localStorageObject.toState);
+// console.log(localStorageObject.toZip);
+// console.log(localStorageObject.toLocationString);
 console.log(localStorage.requiredArrivalTime);
 console.log(localStorageObject.morningRoutineTime); 
 
