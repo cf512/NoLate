33e0a379d22a7055ebe87b68c61da2db0574c2e2
shirano2 <shirano2@naver.com>
2019-03-22 13:16:01 -0500

    google map things

--- assets/javascript/map.js
+++ assets/javascript/map.js
@@ -1,66 +1,146 @@
-$(function() {
-    function calculateRoute(from, to) { 
-        var myOptions = {
-            zoom: 15,
-            center: {lat: 30.27, lng :-97.74}, 
-            mapTypeId: google.maps.MapTypeId.ROADMAP
-        };
-        var transport=localStorage.getItem("transport").toUpperCase();
-        var mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
-        var directionsService = new google.maps.DirectionsService;
-        var directionsDisplay = new google.maps.DirectionsRenderer;
-        var date = new Date();
-        date=date.toDateString()+" "+localStorage.getItem("Morning Routine Time");
+var duration;
+var timeCheck=false;
+var timeCheckFinished=false;
+var date = new Date();
+var transport;
+var myOptions;
+var mapObject;
+var directionsService;
+var directionsDisplay;
+var directionsRequest;
 
-        date=moment(date).add(7, 'days')["_d"];
-       
-        var directionsRequest = {
-            origin: from,
-            destination: to,
-            travelMode: google.maps.DirectionsTravelMode[transport],
-            unitSystem: google.maps.UnitSystem.METRIC,
-            drivingOptions: {
-                departureTime: new Date(date),
-                trafficModel: 'optimistic',
-            }
-        };
+function calculateRoute(from, to) { 
+    myOptions = {
+        zoom: 15,
+        //scrollwheel:  false,
+        center: {lat: 30.27, lng :-97.74}, 
+        mapTypeId: google.maps.MapTypeId.ROADMAP
+    };
+
+    mapObject = new google.maps.Map(document.getElementById("map"), myOptions);
+    directionsService = new google.maps.DirectionsService;
+    directionsDisplay = new google.maps.DirectionsRenderer;
+    
+    transport=localStorage.getItem("transport").toUpperCase();
+    
+    //var reqTime=localStorage.getItem("requiredArrivalTime").split(":");
+
+    //if(!timeCheck) {
+        arrivalText=date.toDateString()+" "+localStorage.getItem("requiredArrivalTime")+":00";
+
+        if(moment(arrivalText,"ddd MMM DD YYYY HH:mm:ss ZZ")<moment()) {
+            arrivalDate=moment(arrivalText,"ddd MMM DD YYYY HH:mm:ss ZZ").add(1, 'days');
+            startTime=moment(arrivalText,"ddd MMM DD YYYY HH:mm:ss ZZ").add(1, 'days');
+        } else {
+            arrivalDate=moment(arrivalText,"ddd MMM DD YYYY HH:mm:ss ZZ");
+            startTime=moment(arrivalText,"ddd MMM DD YYYY HH:mm:ss ZZ");
+        }
+        startTime.subtract(30, 'minutes');
+  //  }
+
+    
+    // arrivalDate=moment(arrivalText,"ddd MMM DD YYYY HH:mm:ss ZZ").add(1, 'days');
+    // startTime=moment(arrivalText,"ddd MMM DD YYYY HH:mm:ss ZZ").add(1, 'days');
+    //date=moment(date,"ddd MMM DD YYYY HH:mm:ss ZZ").add(1, 'days').toDate();
+    //.toDate();
+    //localStorage.getItem("morningRoutineTime");
+
+    //don't use this.
+    //var startTime=arrivalDate;
+
+    //while (!timeCheck) {
+   // while (!timeCheckFinished) {
+   // for(var i=0; i<48; i++) {
+
+//    if (googleCheck(from,to)) {
+//        //return duration;
+
+//        return;
+//    } else {
+//         calculateRoute(from, to);
+//    }
+        googleCheck(from,to);
         
-        directionsService.route(
-            directionsRequest,
-            function(response, status) {
-                console.log(response);
-                if (status == google.maps.DirectionsStatus.OK) {
-                    directionsDisplay.setMap(mapObject);
-                    directionsDisplay.setDirections(response);
-                    directionsDisplay.getMap().setZoom(15);
+        
+   // }
+}
 
-                    $("#googleMapDump p").text("");
-                    $("#googleMapDump p").append("Transport : "+transport);
-                    $("#googleMapDump p").append("<br>From : "+response.routes[0].legs[0].start_address);
-                    $("#googleMapDump p").append("<br>To : "+response.routes[0].legs[0].end_address);
-                    $("#googleMapDump p").append("<br>Total distance : "+response.routes[0].legs[0].distance.text);
-                    $("#googleMapDump p").append("<br>Total time : "+response.routes[0].legs[0].duration.text);
-                    console.log(response);
-                    localStorage.setItem('transitTime', response.routes[0].legs[0].duration.value);
-                    updateAlarmClock();
-                } else {
-                    $("#error").append("Unable to retrieve your route<br />");
-                }  
-            }
-         );
-    }
+function googleCheck(from,to) {
 
-    if (typeof navigator.geolocation == "undefined") {
-        $("#error").text("Your browser doesn't support the Geolocation API");
-        return;
-    }
+  //  if(timeCheck==false) {
+        //console.log(timeCheck);
+        //timeCheck=true;
+        //console.log(timeCheck);
 
-    $("#submitButton").on("click",function(event){
-        event.preventDefault();
-        calculateRoute(localStorage.getItem("fromAddress"), localStorage.getItem("toAddress"));
-    });
+        if(!timeCheck) {
+            directionsRequest = {
+                origin: from,
+                destination: to,
+                travelMode: google.maps.DirectionsTravelMode[transport],
+                unitSystem: google.maps.UnitSystem.METRIC,
+                drivingOptions: {
+                    departureTime: startTime.toDate(),
+                    trafficModel: 'optimistic',
+                }
+            };
+            timeCheck=true;
+        }
 
-    if(checkCookie()) {
-        calculateRoute(localStorage.getItem("fromAddress"), localStorage.getItem("toAddress"));
-    }
-});
+        if (!timeCheckFinished) {
+            timeCheckFinished=true;
+            directionsService.route(
+                directionsRequest,
+                function(response, status) {
+                // console.log(response);
+                    if (status == google.maps.DirectionsStatus.OK) {
+                        var duration=response.routes[0].legs[0].duration.value;
+                        console.log("체크포인트",arrivalDate.diff(startTime, 'seconds')>=duration);
+                        if(arrivalDate.diff(startTime, 'seconds')>=duration) {
+                            console.log("엔드");
+                            directionsDisplay.setMap(mapObject);
+                            directionsDisplay.setDirections(response);
+                            directionsDisplay.getMap().setZoom(15);
+                            //timeCheckFinished=true;
+                            localStorage.setItem('startTime', startTime.subtract(localStorage.getItem("morningRoutineTime"), 'minutes').format('HH:mm'));
+                            localStorage.setItem('transitTime', duration);
+                            updateAlarmClock();
+                            // return true;
+                            return;
+                        } else {
+                            console.log("엔드 엘스");
+                            startTime.subtract(30, 'minutes');
+                            directionsRequest = {
+                                origin: from,
+                                destination: to,
+                                travelMode: google.maps.DirectionsTravelMode[transport],
+                                unitSystem: google.maps.UnitSystem.METRIC,
+                                drivingOptions: {
+                                    departureTime: startTime.toDate(),
+                                    trafficModel: 'optimistic',
+                                }
+                            };
+                            timeCheckFinished=false;
+                            // return false;
+                            googleCheck(from,to);
+                        }
+    
+                        // $("#googleMapDump p").text("");
+                        // $("#googleMapDump p").append("Transport : "+transport);
+                        // $("#googleMapDump p").append("<br>From : "+response.routes[0].legs[0].start_address);
+                        // $("#googleMapDump p").append("<br>To : "+response.routes[0].legs[0].end_address);
+                        // $("#googleMapDump p").append("<br>Total distance : "+response.routes[0].legs[0].distance.text);
+                        // $("#googleMapDump p").append("<br>Total time : "+response.routes[0].legs[0].duration.text);
+    
+                        //
+                        
+                    } else {
+                        $("#error").append("Unable to retrieve your route<br />");
+                    }  
+                }
+            );
+
+        }
+    
+        
+    //}
+}
--- assets/javascript/submit.js
+++ assets/javascript/submit.js
@@ -1,42 +1,53 @@
- var config = {
-        apiKey: "AIzaSyBqq61A0kK_3nScseexY0EAY26DBym3s7c",
-        authDomain: "firstteamproject-16be1.firebaseapp.com",
-        databaseURL: "https://firstteamproject-16be1.firebaseio.com",
-        projectId: "firstteamproject-16be1",
-        storageBucket: "firstteamproject-16be1.appspot.com",
-        messagingSenderId: "546682156707"
-    };
+var config = {
+    apiKey: "AIzaSyBqq61A0kK_3nScseexY0EAY26DBym3s7c",
+    authDomain: "firstteamproject-16be1.firebaseapp.com",
+    databaseURL: "https://firstteamproject-16be1.firebaseio.com",
+    projectId: "firstteamproject-16be1",
+    storageBucket: "firstteamproject-16be1.appspot.com",
+    messagingSenderId: "546682156707"
+};
 
-    firebase.initializeApp(config);
-    var database = firebase.database();
+firebase.initializeApp(config);
+var database = firebase.database();
+
+function setCookie(cname, cvalue, exdays) {
+var d = new Date();
+d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
+var expires = "expires="+d.toUTCString();
+document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
+}
+
+function init(){
+if (typeof navigator.geolocation == "undefined") {
+    $("#error").text("Your browser doesn't support the Geolocation API");
+    return;
+}
+
+$("#submitButton").on("click",function(event){
+    event.preventDefault();
     
-    $(function(){
-   
     var connection=database.ref("/userdata");
+    var con=connection.push($("#nameInput").val());
+    setCookie("commuter"+$("#nameInput").val()+con.key,"true",1);
 
-    function setCookie(cname, cvalue, exdays) {
-        var d = new Date();
-        d.setTime(d.getTime() + (exdays * 24 * 60 * 60 * 1000));
-        var expires = "expires="+d.toUTCString();
-        document.cookie = cname + "=" + cvalue + ";" + expires + ";path=/";
-    }
+    localStorage.clear();
+    localStorage.setItem("name",$("#nameInput").val()); 
+    localStorage.setItem("transport",$("#transport").val());
+    localStorage.setItem("fromAddress",$("#addressFromInput").val()); 
+    localStorage.setItem("toAddress",$("#addressToInput").val()); 
+    localStorage.setItem("requiredArrivalTime", $("#requiredArrivalTime").val());
+    localStorage.setItem("morningRoutineTime", $("#morningRoutineTime").val());
 
-    $("#submitButton").on("click",function(event){
-        event.preventDefault();
-        localStorage.setItem("name",$("#nameInput").val()); 
-        var con=connection.push($("#nameInput").val());
-
-        //????
-        setCookie("commuter"+$("#nameInput").val()+con.key,"true",1);
-        console.log("쿠키"+document.cookie);
-        $("#myModal").modal('hide')
-        $("#bodyWrap").show();
-        $("button.btn").show();
-
-        localStorage.setItem("transport",$("#transport").val());
-        localStorage.setItem("fromAddress",$("#addressFromInput").val()); 
-        localStorage.setItem("toAddress",$("#addressToInput").val()); 
-        localStorage.setItem("requiredArrivalTime", $("#requiredArrivalTime").val());
-        localStorage.setItem("morningRoutineTime", $("#morningRoutineTime").val());
-    });
+    $("#myModal").modal('hide')
+    $("#bodyWrap").show();
+    $("button.btn").show();
+    calculateRoute(localStorage.getItem("fromAddress"), localStorage.getItem("toAddress"));
+    
 });
+
+if(checkCookie()) {
+    calculateRoute(localStorage.getItem("fromAddress"), localStorage.getItem("toAddress"));
+}
+
+initAutocomplete();
+}
